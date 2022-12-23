34b1a1a93ac1a2e608cbe28a831ce7563c0fb9b5
weitude <eric.c.w.lin@gmail.com>
2022-12-23 23:51:31 +0800

    Updates

--- frontend/src/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-  baseURL: "https://weitude-scorecard.up.railway.app/",
-});
-
-export default instance;
-
-// instance.get('/hi').then((data) => console.log(data));
